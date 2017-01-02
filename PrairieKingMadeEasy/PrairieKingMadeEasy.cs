2bedd71dfe4c7b54644d5a13394c9cc9dd63a3ec
EnderHDMC <EnderHDMC@gmail.com>
2017-01-02 16:20:39 +0200

    Using new SMAPI APIs
    Updated the files to use new SMAPI API's as opposed to the old deprecated ones.

--- PrairieKingMadeEasy/PrairieKingMadeEasy.cs
+++ PrairieKingMadeEasy/PrairieKingMadeEasy.cs
@@ -1,48 +1,54 @@
-﻿using System;
+using System;
 using StardewValley;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 
 namespace PrairieKingMadeEasy
 {
-    public class PrairieKingMadeEasy : Mod
-    {
-        public static ModConfig config { get; private set; }
+	// The mod entry point.
+	public class ModEntry : Mod
+	{
+		private ModConfig Config;
+		/*********
+        ** Public methods
+        *********/
+		// Initialise the mod.
+		// Provides methods for interacting with the mod directory, such as reading/writing a config file or custom JSON files.
+		public override void Entry(IModHelper helper)
+		{
+			// read file
+			this.Config = this.Helper.ReadJsonFile<ModConfig>("config.json") ?? new ModConfig();
+			// save file (if needed)
+			this.Helper.WriteJsonFile("config.json", this.Config);
+			GameEvents.UpdateTick += Event_UpdateTick;
+		}
 
-        public override void Entry(params object[] objects)
-        {
-            config = new ModConfig();
-            config = config.InitializeConfig<ModConfig>(base.BaseConfigPath);
-            GameEvents.UpdateTick += Event_UpdateTick;
-        }
+		private void Event_UpdateTick (object sender, EventArgs e)
+		{
+			if (Game1.currentMinigame != null && "AbigailGame".Equals(Game1.currentMinigame.GetType().Name))
+			{
+				Type minigameType = Game1.currentMinigame.GetType();
 
-        private static void Event_UpdateTick (object sender, EventArgs e)
-        {
-            if (Game1.currentMinigame != null && "AbigailGame".Equals(Game1.currentMinigame.GetType().Name))
-            {
-                Type minigameType = Game1.currentMinigame.GetType();
+				if (Config.alwaysInvincible)
+				{
+					minigameType.GetField("playerInvincibleTimer").SetValue(Game1.currentMinigame, 5000);
+				}
 
-                if (config.infiniteLives)
-                {
-                    minigameType.GetField("lives").SetValue(Game1.currentMinigame, 99);
-                }
+				if (Config.infiniteCoins)
+				{
+					minigameType.GetField("coins").SetValue(Game1.currentMinigame, 90);
+				}
 
-                if (config.infiniteCoins)
-                {
-                    minigameType.GetField("coins").SetValue(Game1.currentMinigame, 99);
-                }
+				if (Config.infiniteLives)
+				{
+					minigameType.GetField("lives").SetValue(Game1.currentMinigame, 20);
+				}
 
-                if (config.rapidFire)
-                {
-                    minigameType.GetField("shootingDelay").SetValue(Game1.currentMinigame, 25);
-                }
-
-                if (config.alwaysInvincible)
-                {
-                    minigameType.GetField("playerInvincibleTimer").SetValue(Game1.currentMinigame, 5000);
-                }
-            }
-        }
-      
-    }
+				if (Config.rapidFire)
+				{
+					minigameType.GetField("shootingDelay").SetValue(Game1.currentMinigame, Config.fireDelay);
+				}
+			}
+		}
+	}
 }

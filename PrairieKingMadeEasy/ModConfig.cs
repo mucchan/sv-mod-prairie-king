1bfd508f219de7e046b971b2a0e8d3a062dcca0a
EnderHDMC <EnderHDMC@gmail.com>
2017-01-02 16:27:28 +0200

    Using new SMAPI APIs and FireDelay setting
    Now using new SMAPI APIs and a new FireDelay setting has been added.

--- PrairieKingMadeEasy/ModConfig.cs
+++ PrairieKingMadeEasy/ModConfig.cs
@@ -2,21 +2,21 @@ using StardewModdingAPI;
 
 namespace PrairieKingMadeEasy
 {
-    public class ModConfig
-    {
-        public bool alwaysInvincible { get; set; }
-        public bool infiniteCoins { get; set; }
-        public bool infiniteLives { get; set; }
-        public bool rapidFire { get; set; }
+	public class ModConfig
+	{
+		public bool alwaysInvincible { get; set; }
+		public bool infiniteCoins { get; set; }
+		public bool infiniteLives { get; set; }
+		public bool rapidFire { get; set; }
 		public int fireDelay { get; set; }
 
 		public ModConfig()
-        {
-            this.alwaysInvincible = false;
-            this.infiniteCoins = false;
-            this.infiniteLives = false;
-            this.rapidFire = false;
+		{
+			this.alwaysInvincible = false;
+			this.infiniteCoins = false;
+			this.infiniteLives = false;
+			this.rapidFire = false;
 			this.fireDelay = 25;
-        }
-    }
+		}
+	}
 }
